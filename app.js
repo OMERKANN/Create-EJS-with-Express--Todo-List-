27daa12fffe92dde908de9f70cd73787795596bd
Omer Kan <omerkan93@gmail.com>
2022-11-23 14:58:36 +0300

    creat css

--- app.js
+++ app.js
@@ -2,26 +2,47 @@ const express = require('express')
 const bodyParser = require("body-parser");
 const app = express()
 
+let ıtemList =["Shoping", "Codding","Go to GYM"];
+
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
 
-    var numberDay = today.getDay();
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let today  = new Date();
+
+    
+    today.toLocaleDateString("en-US", options);
+
+
+    let numberDay = today.getDay();
  
-    const daysOfWeek = ["Pazartesi", "Salı", "Çarşamba", "perşembe", "Cuma", "Cumartesi", "Pazar"]
+    const daysOfWeek = ["Pazar","Pazartesi", "Salı", "Çarşamba", "perşembe", "Cuma", "Cumartesi"]
 
-    var day = daysOfWeek[numberDay];
+    let day = daysOfWeek[numberDay];
 
-    res.render("index", {goDay: day});
-      
-  
+    res.render("index", {goDay: day, addItems: ıtemList});
+
+    
+
 });
+
+app.post("/", (req, res) => {
+
+    let newItem = req.body.newItem;
+
+    ıtemList.push(newItem);
+
+    res.redirect("/")
+    
+})
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`)
+  
 })
