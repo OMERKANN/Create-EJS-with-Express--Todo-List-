1aeb89b3afd45c5604da2398e9cc6707a0b6acbc
Omer <omerkan93@gmail.com>
2022-12-11 22:59:19 +0300

    MongoosVersionCreate

--- app.js
+++ app.js
@@ -1,58 +1,87 @@
-const express = require('express')
+const express = require('express') 
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const app = express()
 
-let ıtemList =["Shoping", "Codding","Go to GYM"];
-let workItems = [];
+// ejs view
+app.set('view engine', 'ejs');
 
+//body-parser express public file
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs');
+// create todoDB in mongoose 1
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+ // {userNewUrlParser : true}for don't show erorr version
+
+// create item Schema 2
+
+const itemsSchema = {
+  name : "string"
+};
+
+// create mongoose model for schema 3
+
+
+const Item = mongoose.model('Item', itemsSchema);
+
+//create item array
+
+const item1 = new Item({
+  name: "Welcome top your todolist",
+}) 
+
+const defaultItems =[item1, item2, item3];
+
+// day of the week creat and call ejs
 
 app.get('/', (req, res) => {
 
+  
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var today = days[new Date().getDay()];
 
-let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-let today  = new Date();
+    //find mongoose model for show page "/" item lsit
+    Item.find({}, function(err, foundItems){
 
-    
-    today.toLocaleDateString("en-US", options);
+      
+      if(foundItems.length === 0){
 
+        // item arry insert Item Model
+        Item.insertMany(defaultItems, function(err) {
+          if(err)
+          {console.log("items is error")
+        }else{
+          console.log("good job")
+        }
+        });
+        res.redirect("/")
+      }else{
+        res.render("index", {listTitle: today, addItems: foundItems});
+      }
 
-    let numberDay = today.getDay();
- 
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = daysOfWeek[numberDay];
-
-    res.render("index", {listTitle: day, addItems: ıtemList});
-
-    
+      
+    });
 
 });
 
+// add item newItem to do page 
 app.post("/", (req, res) => {
 
-    console.log(req.body)
+    const itemName = req.body.newItem;
+     
+    const item = new Item({
+      name: itemName
+    })
 
-    let ıtem = req.body.newItem;
-
-    if(req.body.list === "Work"){
-      workItems.push(ıtem);
-
-      res.redirect("/work");
-    } else
-
-    ıtemList.push(ıtem);
+    item.save();
 
     res.redirect("/")
-    
-})
+});
 
+// add workItems to page
 app.get('/Work', (req, res) => {
-
-
   res.render("index", {listTitle: "Work", addItems: workItems });
   
   });
